92cacc5feff8fe5e43361ccf0fc350a2820a1e85
yeachan <dus001228@naver.com>
2024-03-27 15:38:02 +0900

    Rename

--- Wallet_app_UI/lib/main.dart
+++ Wallet_app_UI/lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
-import 'package:toonflix/widgets/button.dart';
-import 'package:toonflix/widgets/currency_card.dart';
+import 'package:wallet_app_UI/widgets/button.dart';
+import 'package:wallet_app_UI/widgets/currency_card.dart';
 
 class Player {
   String? name;
